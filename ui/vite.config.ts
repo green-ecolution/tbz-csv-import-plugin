c2bde159b955a14465194944f564eb4e3792b80c
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-12-13 01:30:23 +0100

    feat: change ui plugin to needed specifications

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: "demo_plugin",
+      name: "csv-import",
       filename: "plugin.js",
       exposes: {
-        "./RemoteARoot": "./src/App.tsx",
+        "./app": "./src/App.tsx",
       },
       shared: ["react", "react-dom", "@green-ecolution/plugin-interface"],
     }),
   ],
   base: "",
   build: {
-    target: 'esnext'
-  }
-})
+    target: "esnext",
+  },
+});
